43c36bb3faeea886d4ef191c5acebb08b8562e22
maydinn <muhammedaydin9214@gmail.com>
2023-10-28 18:32:53 +0200

    all

--- pages/2_Wirtschaftliche Indikatoren.py
+++ pages/2_Wirtschaftliche Indikatoren.py
@@ -63,8 +63,10 @@ if col1_button.button("Monatlich"):
 if col2_button.button("Jahrlich"):
     st.session_state.button1_selected = False
     st.session_state.button2_selected = True
+    
+st.write('hey')
 if st.session_state.button1_selected: 
-st.write('hey')    
+    
     df = pd.read_csv("data_month.csv", index_col=0)
 
     st.subheader('Wirtschaftliche Indikatoren auf monatlich Basis')
